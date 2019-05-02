47b14f77a004f0161c13f854912a81525353e083
Valeria Duran Ruiz <valeria.do@gmail.com>
2019-05-02 11:33:53 -0500

    Fit buttons into SE size screen programmatically

--- Mindfulness/ViewController.m
+++ Mindfulness/ViewController.m
@@ -33,14 +33,14 @@
     [circlePath addArcWithCenter:self.mainHomeView.center radius: 150 startAngle: 0 endAngle: 2 * M_PI clockwise:TRUE];
     //draw  main circle
     circleShapeLayer.path = circlePath.CGPath;
-    //circleShapeLayer.strokeColor = UIColor.blueColor.CGColor;
-    //circleShapeLayer.lineWidth = 10;
+//    circleShapeLayer.strokeColor = UIColor.blueColor.CGColor;
+//    circleShapeLayer.lineWidth = 10;
     circleShapeLayer.fillColor = [[UIColor clearColor] CGColor];
     
     //Add image buttons to view
-    UIView *breathView = [[UIView alloc] initWithFrame:CGRectMake( circlePath.currentPoint.x - 50, circlePath.currentPoint.y - 50, 100, 100)];
+    UIView *breathView = [[UIView alloc] initWithFrame:CGRectMake( circlePath.currentPoint.x - 100, circlePath.currentPoint.y - 50, 100, 100)];
     UIView *meditateView = [[UIView alloc] initWithFrame:CGRectMake( circlePath.currentPoint.x - 200, circlePath.currentPoint.y + 150, 100, 100)];
-    UIView *journalView = [[UIView alloc] initWithFrame:CGRectMake( circlePath.currentPoint.x - 350, circlePath.currentPoint.y - 50, 100, 100)];
+    UIView *journalView = [[UIView alloc] initWithFrame:CGRectMake( circlePath.currentPoint.x - 300, circlePath.currentPoint.y - 50, 100, 100)];
     UIView *playView = [[UIView alloc] initWithFrame:CGRectMake( circlePath.currentPoint.x - 200, circlePath.currentPoint.y - 250, 100, 100)];
     
     breathView.layer.cornerRadius = 50;
@@ -59,11 +59,11 @@
     [self.mainHomeView addSubview:playView];
     
     //Add Labels to Views
-    UILabel *breathLabel = [[UILabel alloc] initWithFrame:CGRectMake(breathView.center.x - 40, breathView.center.y - 10, 80, 30)]; //[UILabel new];
+    UILabel *breathLabel = [[UILabel alloc] initWithFrame:CGRectMake(breathView.center.x - 40, breathView.center.y - 10, 80, 30)];
     UILabel *meditateLabel = [[UILabel alloc] initWithFrame:CGRectMake(meditateView.center.x - 40, meditateView.center.y - 10, 80, 30)];
     UILabel *journalLabel = [[UILabel alloc] initWithFrame:CGRectMake(journalView.center.x - 40, journalView.center.y - 10, 80, 30)];
     UILabel *playLabel = [[UILabel alloc] initWithFrame:CGRectMake(playView.center.x - 40, playView.center.y - 10, 80, 30)];
-//    breathLabel.textColor = UIColor.blackColor;
+
     breathLabel.text = @"Breath";
     meditateLabel.text = @"Meditate";
     journalLabel.text = @"Journal";
@@ -78,10 +78,7 @@
      meditateLabel.textAlignment = NSTextAlignmentCenter;
      journalLabel.textAlignment = NSTextAlignmentCenter;
      playLabel.textAlignment = NSTextAlignmentCenter;
-    //[breathLabel setFont:[UIFont fontWithName: @"Arial" size:16]];
-    
-    //breathLabel.center = CGPointMake(breathView.center.x, breathView.center.y);
-    
+   
     [self.mainHomeView addSubview:breathLabel];
     [self.mainHomeView addSubview:meditateLabel];
     [self.mainHomeView addSubview:journalLabel];
