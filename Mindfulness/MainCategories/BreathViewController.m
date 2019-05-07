383c3f5a372016aa7f6aa813e9c71b934bf5aa35
Valeria Duran Ruiz <valeria.do@gmail.com>
2019-05-07 16:59:21 -0500

    Breath circular paths grey base and animating rounded corner layer on top

--- Mindfulness/MainCategories/BreathViewController.m
+++ Mindfulness/MainCategories/BreathViewController.m
@@ -25,21 +25,26 @@ CAShapeLayer *circleShapeLayer;
     UIView *breathMainView = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];                              //CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
     [breathMainView setBackgroundColor:UIColor.whiteColor];
     [self.view addSubview:breathMainView];
-    
     //draw  main circle
-    circleShapeLayer = [CAShapeLayer layer];
-    [breathMainView.layer addSublayer:circleShapeLayer];
     UIBezierPath* circlePath = [UIBezierPath bezierPath];
     [circlePath addArcWithCenter:breathMainView.center radius: 100 startAngle: -(M_PI / 2) endAngle: 2 * M_PI clockwise:YES];
+    //grey base track
+    CAShapeLayer *track = [CAShapeLayer layer];
+    track.path = circlePath.CGPath;
+    track.strokeColor = UIColor.grayColor.CGColor;
+    track.lineWidth = 10;
+    track.fillColor = [[UIColor clearColor] CGColor];
+    [breathMainView.layer addSublayer:track];
+    //main moving track
+    circleShapeLayer = [CAShapeLayer layer];
     circleShapeLayer.path = circlePath.CGPath;
-    circleShapeLayer.fillColor = [[UIColor clearColor] CGColor];
     circleShapeLayer.strokeColor = UIColor.blueColor.CGColor;
     circleShapeLayer.lineWidth = 10;
+    circleShapeLayer.fillColor = [[UIColor clearColor] CGColor];
     circleShapeLayer.lineCap = kCALineCapRound;
-    
+    [breathMainView.layer addSublayer:circleShapeLayer];
     //Add gesture recognizer
     UITapGestureRecognizer *breathTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleBreathTap:)];
-    
     [breathMainView addGestureRecognizer: breathTap];
     
 }
@@ -48,14 +53,12 @@ CAShapeLayer *circleShapeLayer;
     //CGPoint location = [recognizer locationInView:[recognizer.view superview]];
     NSLog(@"BREATH Button TAPPED");
     CABasicAnimation *basicAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
-    basicAnimation.fromValue = 0;
-    basicAnimation.toValue = [NSValue valueWithCGPoint: circleShapeLayer.position]; //1;
+    basicAnimation.toValue = 0;//[NSValue valueWithCGPoint: circleShapeLayer.position];
+    basicAnimation.fromValue = [NSValue valueWithCGPoint: circleShapeLayer.position]; //1;
     basicAnimation.duration = 2;
-    basicAnimation.fillMode = kCAFillModeForwards;
-    [basicAnimation isRemovedOnCompletion];
-    [circleShapeLayer addAnimation:basicAnimation forKey:@"urSoBasic"];
-    
-    
+    //basicAnimation.fillMode = kCAFillModeBackwards;
+   // [basicAnimation isRemovedOnCompletion];
+    [circleShapeLayer addAnimation:basicAnimation forKey:basicAnimation.keyPath];
 }
 
 
