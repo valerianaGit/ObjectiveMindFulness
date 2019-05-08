2012ad492957f491df73b48787fb45025daa63ae
Valeria Duran Ruiz <valeria.do@gmail.com>
2019-05-07 21:19:06 -0500

    Download file and calculate percentage to represent it in a circular animation

--- Mindfulness/MainCategories/BreathViewController.m
+++ Mindfulness/MainCategories/BreathViewController.m
@@ -16,13 +16,15 @@ CAShapeLayer *circleShapeLayer;
 
 @implementation BreathViewController
 
+NSString *urlString = @"https://firebasestorage.googleapis.com/v0/b/firestorechat-e64ac.appspot.com/o/intermediate_training_rec.mp4?alt=media&token=e20261d0-7219-49d2-b32d-367e1606500c";
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setupViews];
 }
 
 -(void) setupViews {
-    UIView *breathMainView = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];                           
+    UIView *breathMainView = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
     [breathMainView setBackgroundColor:UIColor.whiteColor];
     [self.view addSubview:breathMainView];
     //draw  main circle
@@ -49,14 +51,43 @@ CAShapeLayer *circleShapeLayer;
     
 }
 
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)location {
+    NSLog(@"finished downloading file");
+}
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didWriteData:(int64_t)bytesWritten totalBytesWritten:(int64_t)totalBytesWritten totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite {
+    //Handling data percentage of url download 
+     CGFloat percentage = (double)totalBytesWritten/(double)totalBytesExpectedToWrite;
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+       circleShapeLayer.strokeEnd = percentage;
+        
+    });
+    
+    
+    
+}
+-(void)beginDownloadingFile {
+     NSLog(@"downloading file");
+    circleShapeLayer.strokeEnd = 0; //starting stroke here avoids the extra animation of stroke going to 0
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSOperationQueue *operationQueue = [NSOperationQueue mainQueue];
+    NSURLSession *urlSession = [NSURLSession sessionWithConfiguration:configuration delegate:self delegateQueue:operationQueue];
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLSessionDownloadTask *dowloadTask = [ urlSession downloadTaskWithURL:url];
+    [dowloadTask resume];
+}
+
 - (void) handleBreathTap:(UITapGestureRecognizer *)recognizer {
     NSLog(@"BREATH Button TAPPED");
-    CABasicAnimation *basicAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
-    basicAnimation.toValue = 0;
-    basicAnimation.fromValue = [NSValue valueWithCGPoint: circleShapeLayer.position];
-    basicAnimation.duration = 2;
-    [circleShapeLayer addAnimation:basicAnimation forKey:basicAnimation.keyPath];
+    [self beginDownloadingFile];
+//    CABasicAnimation *basicAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
+//    basicAnimation.toValue = 0;
+//    basicAnimation.fromValue = [NSValue valueWithCGPoint: circleShapeLayer.position];
+//    basicAnimation.duration = 2;
+//    [circleShapeLayer addAnimation:basicAnimation forKey:basicAnimation.keyPath];
 }
+
 
 
 @end
